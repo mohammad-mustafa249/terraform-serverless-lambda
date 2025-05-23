4dda0c841af13e52904af6a18c8c4db8da390484
mohammad-mustafa249 <aim.mustafa249@gmail.com>
2025-05-23 16:25:11 +0530

    Fixed

--- main.tf
+++ main.tf
@@ -85,6 +85,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_lambda_function" "container_lambda" {
  function_name = "my-container-lambda"
  package_type = "Image"
